8c6503be230465d2f6c7ccb67e193a4ae3b516af
Khushwant sharma <132260675+Khushwant12@users.noreply.github.com>
2025-11-20 10:43:52 +0530

    code committed

--- src/MobileAppPages/DailyAssignments/AssignmentSummary/Pages/AssignmentSummary/AssignmentSummary.jsx
+++ src/MobileAppPages/DailyAssignments/AssignmentSummary/Pages/AssignmentSummary/AssignmentSummary.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import styles from '../../Styles/AssignmentSummary/AssignmentSummary.module.css'
+import { ArrowLeft } from 'lucide-react'
 
 const AssignmentSummary = () => {
-  return (
-    <div></div>
-  )
+    return (
+        <div className={styles.pageWrapper}>
+            <div className={styles.mobileView}>
+                <div className={styles.header}>
+                    <button
+                        className={styles.backButton}
+                    >
+                        <ArrowLeft size={22} />
+                    </button>
+
+                    <h1 className={styles.title}>Assignment Summary</h1>
+                </div>
+                
+            </div>
+        </div>
+    )
 }
 
 export default AssignmentSummary
