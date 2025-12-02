3fb5119fcf961e5a9e7040dbfbbe9c7ac8e7c9fe
Khushwant sharma <132260675+Khushwant12@users.noreply.github.com>
2025-12-02 15:10:57 +0530

    code committed

--- src/MobileAppPages/Tasks/Action/AddTask/AddTaskAction.js
+++ src/MobileAppPages/Tasks/Action/AddTask/AddTaskAction.js
@@ -2,11 +2,14 @@ export const handleChange = (type, value, setDisplayName, setError) => {
     if (type === "name") {
         setDisplayName(value);
 
-        if (value.trim() !== "") {
+        if (value.trim() === "") {
+            setError("Please provide display name of task.");
+        } else {
             setError("");
         };
     };
 };
+
 
 export const handleSaveTasks = (displayName, setError) => {
     if (displayName.trim() === "") {
