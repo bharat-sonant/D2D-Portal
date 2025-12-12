ff4a205782e45f4f5005cf2295e5c805b400bb9d
Ansh Lahari <anshwevois@gmail.com>
2025-12-12 16:18:45 +0530

    code committed for adding client file

--- src/createClient.js
+++ src/createClient.js
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 
 export const supabase= createClient(
-    "https://hykirowrddtvnbgpqxet.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5a2lyb3dyZGR0dm5iZ3BxeGV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0MzMzMjUsImV4cCI6MjA4MTAwOTMyNX0.ArmtqUkH3EVSfraoey_Qab_xgUCoCjFmtqdnjW1Rl_I")
+    "https://qaqlxzjlhgmnuurmdasi.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhcWx4empsaGdtbnV1cm1kYXNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzAyMDMsImV4cCI6MjA4MTEwNjIwM30.ycC6uemznHsWAnOzH4Hge2M1Atj24z5OtKA8VFTrecM")
