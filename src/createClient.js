4df278644ff96922034baf06e14e5a54887a4225
Ansh Lahari <anshwevois@gmail.com>
2025-12-12 17:15:56 +0530

    code committed

--- src/createClient.js
+++ src/createClient.js
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 
 export const supabase= createClient(
-    "https://qaqlxzjlhgmnuurmdasi.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhcWx4empsaGdtbnV1cm1kYXNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzAyMDMsImV4cCI6MjA4MTEwNjIwM30.ycC6uemznHsWAnOzH4Hge2M1Atj24z5OtKA8VFTrecM")
+    "https://tayzauotsjxdgvfadcby.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRheXphdW90c2p4ZGd2ZmFkY2J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzcwMzksImV4cCI6MjA4MTExMzAzOX0.LPufHiX8rNi05qIv0ji4YjAbn8ZW_dMaWJRP31JIcqQ")
