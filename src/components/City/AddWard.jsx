434be2eff07b73f395209508509bf4d60fd6c555
Anil Sharma <100846183+anillkumarsharma@users.noreply.github.com>
2025-12-25 15:26:50 +0530

    code committed

---
+++ src/components/City/AddWard.jsx
@@ -0,0 +1,117 @@
+import {useEffect, useMemo, useState } from "react";
+import dayjs from "dayjs";
+import { FaSpinner } from "react-icons/fa";
+import { images } from "../../assets/css/imagePath";
+import styles from "../../assets/css/modal.module.css";
+import {saveWardAction } from "../../Actions/City/cityAction";
+
+const AddWard = (props) => {
+  const initialForm = {
+    CityId:0,
+    Ward: "",
+  };
+
+  const [loading, setLoading] = useState(false);
+  const [wardNumberError,setWardNumberError]=useState("");
+  const [form, setForm] = useState(initialForm);
+
+   useEffect(() => {
+      const handleKeyDown = (e) => {
+        if (e.key === "Enter" && !loading) {
+          e.preventDefault();
+          handleSave();
+        }
+      };
+      window.addEventListener("keydown", handleKeyDown);
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+      };
+    
+  }, [form,loading]);
+
+  const handleChange = (e) => {
+      setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSave = async () => {
+    saveWardAction(form,props.selectedCity?.CityId,setLoading,setWardNumberError,resetStateValues);
+  };
+
+
+
+  const resetStateValues=()=>{
+    setForm(initialForm);
+    setWardNumberError("")
+    props.setOpenAddWardPopUp(false);
+    setLoading(false);
+    props?.setOnEdit(false);
+  }
+
+
+  return (
+    <div className={styles.overlay} aria-modal="true" role="dialog">
+      <div className={styles.modal}>
+        <div className={styles.actionBtn}>
+          <p className={styles.headerText}>
+            {props?.onEdit ? "Update" : "Add"} Ward
+          </p>
+          <button
+            className={styles.closeBtn}
+            onClick={() => {
+              resetStateValues();
+            }}
+            aria-label="Close"
+          >
+            <img
+              src={images.iconClose}
+              className={styles.iconClose}
+              title="Close"
+              alt="close"
+            />
+          </button>
+        </div>
+
+        <div className={styles.modalBody}>
+
+             <div className={styles.textboxGroup}>
+            <div className={styles.textboxMain}>
+              <div className={styles.textboxLeft}>Ward</div>
+              <div className={styles.textboxRight}>
+                <input
+                  type="text"
+                  className={`form-control ${styles.formTextbox}`}
+                  placeholder="Enter ward name"
+                  name="Ward"
+                  value={form.Ward}
+                  onChange={handleChange}
+                  disabled={Boolean(props?.onEdit && Object.keys(props?.onEdit).length)}
+                />
+              </div>
+            </div>
+            {wardNumberError && (
+              <div className={`${styles.invalidfeedback}`}>{wardNumberError}</div>
+            )}
+          </div>
+          
+          <button
+            type="button"
+            className={`mt-3 ${styles.btnSave}`}
+            onClick={handleSave}
+          >
+            {loading ? (
+              <div className={styles.Loginloadercontainer}>
+                <FaSpinner className={styles.spinnerLogin} />
+                <span className={styles.loaderText}>Please wait...</span>
+              </div>
+            ) : props.onEdit ? (
+              "Update"
+            ) : (
+              "Save"
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddWard;
---
+++ src/components/City/WardList.jsx
@@ -0,0 +1,68 @@
+import { images } from "../../assets/css/imagePath";
+import style from "../../assets/css/City/wardList.module.css"
+
+const WardList = (props) => {
+ 
+    const wardList = [];
+
+
+  return (
+    <div className={style.Detailscard}>
+      <div className={style.card_header}>
+        <h5 className={style.heading}> </h5>
+        
+          <div className="d-flex justify-content-center align-items-center">
+            <button
+              className={`btn ${style.custom_AddDesignation_btn} p-0`}
+              onClick={()=>{props.setOpenAddWardPopUp(true)}}
+            >
+              +
+            </button>
+          </div>
+      
+      </div>
+
+      <div className={style.Scroll_List}>
+        {wardList&& wardList.length > 0 ? (
+          <ul className={style.listLine}>
+            {wardList.map((ward, index) => (
+              <li key={index} className={style.list_item}>
+                <span className={style.designationName}>
+                  {" "}
+                  {ward.name}
+                </span>
+                <div className={style.countEdit}>
+                
+                    <img
+                      src={images.iconEdit}
+                      className={`${style.edit_icon}`}
+                     
+                    />
+           
+                 
+                  
+                      <img
+                        src={images.iconDeleted}
+                        className={style.edit_icon}
+                        width="13px"
+                        
+                      />
+                    
+                  
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className={style.dropdownItemNot}>
+            <img src={images.imgComingSoon} className={`${style.foundNot}`} />
+            No ward found
+          </div>
+        )}
+      </div>
+      
+    </div>
+  );
+};
+
+export default WardList;
