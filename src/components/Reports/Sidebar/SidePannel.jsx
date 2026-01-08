d184dd98ae67ec3b9ee841dd8a492396d2c24aa3
Shubham <shubham.sonant@gmail.com>
2026-01-08 15:53:23 +0530

    code committed

--- src/components/Reports/Sidebar/SidePannel.jsx
+++ src/components/Reports/Sidebar/SidePannel.jsx
@@ -1,22 +1,32 @@
 import style from "../../../Style/Reports_Style/SidePannel/SidePannel.module.css";
-
+import { File } from "lucide-react";
 const SidePannel = ({ selectedReport, setSelectedReport }) => {
-  const menuItems = ["Daily Work Report"];
+  const menuItems = [
+  "Daily Work Report",
+];
 
   return (
-      <div className={style.menuList}>
-        {menuItems.map((item, index) => (
+    <div className={style.menuList}>
+      {menuItems.map((item, index) => (
+        <>
           <div
             key={index}
-            className={`${style.menuItem} ${
-              selectedReport === item ? style.active : ""
-            }`}
+            className={`${style.menuItem} `}
             onClick={() => setSelectedReport(item)}
           >
+            <div
+              className={`${style.menuIcon} ${
+                selectedReport === item ? style.active : ""
+              }`}
+            >
+              <File />
+            </div>
+
             {item}
           </div>
-        ))}
-      </div>
+        </>
+      ))}
+    </div>
   );
 };
 
