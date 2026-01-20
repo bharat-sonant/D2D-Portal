e7cc1282da75219a3bfdb1838c326454cbfca7d4
Khushwant sharma <132260675+Khushwant12@users.noreply.github.com>
2026-01-20 13:20:45 +0530

    code committed

--- src/router/ProtectedRouter/ProtectedRouter.js
+++ src/router/ProtectedRouter/ProtectedRouter.js
@@ -1,14 +1,18 @@
+
 import { Navigate } from "react-router-dom";
 
 const ProtectedRouter = ({ children }) => {
     let isLogin = localStorage.getItem("isLogin");
+   
+    // let loginDate = localStorage.getItem("loginDate");
+    // let todayDate = dayjs().format("DD/MM/YYYY");
+    // let isDateMatch = loginDate === todayDate;
 
     if (isLogin === "success") {
         return children;
     } else {
-        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-        return <Navigate to={isMobile ? "/login" : "/"} replace />;
+        return <Navigate to={"/"} />;
     }
 }
 
-export default ProtectedRouter;
+export default ProtectedRouter
