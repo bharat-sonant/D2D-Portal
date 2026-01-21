d56a2fdc9e5e5d3b6ebeb0fde43c770e48b7c747
Shubham <shubham.sonant@gmail.com>
2026-01-21 14:21:07 +0530

    code committed

--- src/MobileAppPages/Settings/Component/Penalties/Penalties.jsx
+++ src/MobileAppPages/Settings/Component/Penalties/Penalties.jsx
@@ -1,23 +1,49 @@
-import styles from "../../Pages/Settings.module.css";
+// import styles from "../../Pages/Settings.module.css";
+// import { togglePenalties } from "../../Action/Penalties/PenaltiesAction";
+// import { Settings, Shield, Eye, Calendar, Image, Save, ToggleLeft, ToggleRight } from 'lucide-react';
+
+// const Penalties = (props) => {
+//   return (
+//     <div>
+//       <div className={styles.card}>
+//         <div className={styles.toggleWrapper}>
+//           <div className={`${styles.cardIcon} ${styles.purpleGradient}`}>
+//             <Shield size={24} />
+//           </div>
+//           <label className={styles.toggleLabel}>Penalties Via Web</label>
+//           <div
+//             className={`${styles.toggleSwitch} ${
+//               props.isPenaltiesOn ? styles.on : styles.off
+//             }`}
+//             onClick={() => togglePenalties(props)}
+//           >
+//             <div className={styles.toggleCircle}>
+//               {props.isPenaltiesOn ? "ON" : "OFF"}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Penalties;
+
+import style from "../../Style/Settings.module.css";
 import { togglePenalties } from "../../Action/Penalties/PenaltiesAction";
-import { Settings, Shield, Eye, Calendar, Image, Save, ToggleLeft, ToggleRight } from 'lucide-react';
 
 const Penalties = (props) => {
   return (
     <div>
-      <div className={styles.card}>
-        <div className={styles.toggleWrapper}>
-          <div className={`${styles.cardIcon} ${styles.purpleGradient}`}>
-            <Shield size={24} />
-          </div>
-          <label className={styles.toggleLabel}>Penalties Via Web</label>
+      <div className={style.card}>
+        {/* <h3 className={style.cardTitle}>Penalties via Web</h3> */}
+        <div className={style.toggleWrapper}>
+          <label className={style.toggleLabel}>Penalties Via Web</label>
           <div
-            className={`${styles.toggleSwitch} ${
-              props.isPenaltiesOn ? styles.on : styles.off
-            }`}
+            className={`${style.toggleSwitch} ${props.isPenaltiesOn ? style.on : style.off}`}
             onClick={() => togglePenalties(props)}
           >
-            <div className={styles.toggleCircle}>
+            <div className={style.toggleCircle}>
               {props.isPenaltiesOn ? "ON" : "OFF"}
             </div>
           </div>
