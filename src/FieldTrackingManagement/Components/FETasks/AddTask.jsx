e38024c206c21da4e80adf76abe33812cb973c56
Govindsharma123 <govindsharma12112003@gmail.com>
2026-01-23 16:48:34 +0530

    Add Task page

---
+++ src/FieldTrackingManagement/Components/FETasks/AddTask.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react'
+import styles from './AddTask.module.css'
+import ErrorMessage from '../../../components/ErrorMessage/ErrorMessage';
+
+const AddTask = ({taskName, setTaskName, description, setDescription, setOpenCanvas, isEdit,setIsEdit,setEditIndex, onSave}) => {
+  
+  const [taskError, setTaskError] = useState("");
+  const [desError, setDesError] = useState("");
+
+  const handleClose = () => {
+    setOpenCanvas(false);
+    setIsEdit(false);
+    setEditIndex(null)
+  };
+
+const validate = () => {
+  let isValid = true;
+
+  if (!taskName.trim()) {
+    setTaskError("Task name is required");
+    isValid = false;
+  } else {
+    setTaskError("");
+  }
+
+  if (!description.trim()) {
+    setDesError("Description is required");
+    isValid = false;
+  } else {
+    setDesError("");
+  }
+
+  return isValid;
+};
+
+  const handleSave = () => {
+    if (!validate()) return;
+
+    onSave({
+      taskName,
+      description
+    });
+    resetForm();
+  }
+
+  const resetForm = () => {
+    setTaskName("");
+    setDescription("");
+    setOpenCanvas(false);
+  }
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <div className={styles.header}>
+          <h2>{isEdit ? 'Update Task' : 'Add New Task'}</h2>
+          <button className={styles.closeBtn} onClick={handleClose}>
+            ✕
+          </button>
+        </div>
+
+        <div className={styles.body}>
+          <div className={styles.formGroup}>
+            <label>Task Name</label>
+            <input
+              type="text"
+              placeholder="Enter task name"
+              value={taskName}
+              onChange={(e) => {
+                setTaskName(e.target.value)
+                setTaskError("")
+              }}
+            />
+            {taskError && ( 
+              <ErrorMessage message={taskError}/>
+            )}
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Description</label>
+            <textarea
+              placeholder="Enter task description"
+              rows={5}
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value)
+                setDesError("");
+              }}
+            />
+            {desError && (
+            <ErrorMessage message={desError}/>
+            )}
+          </div>
+        </div>
+
+        <div className={styles.footer}>
+          <button className={styles.cancelBtn} onClick={handleClose}>
+            Cancel
+          </button>
+          <button className={styles.saveBtn} onClick={handleSave}>
+            {isEdit ? 'Update Task' : "Save Task"}
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddTask
--- src/FieldTrackingManagement/Pages/FETasks/FETasks.jsx
+++ src/FieldTrackingManagement/Pages/FETasks/FETasks.jsx
@@ -1,10 +1,113 @@
-import React from 'react'
+import React, { useState } from 'react'
+import GlobalStyles from '../../../assets/css/globleStyles.module.css'
+import AddTask from '../../Components/FETasks/AddTask';
+import style from './FETasks.module.css'
+import { Pen } from 'lucide-react';
 
 const FETasks = () => {
+  const [openCanvas, setOpenCanvas] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [taskName, setTaskName] = useState("");
+  const [description, setDescription] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleOpenModal = () => {
+    console.log('open')
+    setOpenCanvas(true);
+  }
+
+  const handleSaveTask = (taskData) => {
+    if(isEdit){
+      setTasks((prev) => 
+        prev.map((task,index) => 
+        index === editIndex
+          ? {...task, ...taskData}
+          : task
+    ))
+    }else{
+      setTasks((prev) => [
+      ...prev,
+      {
+        id: Date.now(),
+        ...taskData,
+      },
+    ])
+    }
+    setEditIndex(null)
+    setIsEdit(false)
+  }
+
+  const handleEdit = (index) => {
+    const selectedTask = tasks[index];
+    setIsEdit(true)
+    setEditIndex(index);
+
+    setTaskName(selectedTask.taskName);
+    setDescription(selectedTask.description)
+
+    setOpenCanvas(true)
+  }
+
   return (
-    <div>
-        Field Executive Tasks Page
-    </div>
+    <>
+        <div className={GlobalStyles.floatingDiv}>
+          <button
+            className={GlobalStyles.floatingBtn}
+            onClick={handleOpenModal}
+          >
+            +
+          </button>
+        </div>
+
+        {/* Tasks Table */}
+      <div className={style.container}>
+        <h2 className={style.title}>Tasks</h2>
+
+        {tasks.length === 0 ? (
+          <p className={style.empty}>No tasks added yet</p>
+        ) : (
+          <table className={style.table}>
+            <thead>
+              <tr>
+                <th>S. No</th>
+                <th>Task Name</th>
+                <th>Description</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task, index) => (
+                <tr key={task.id}>
+                  <td>{index + 1}</td>
+                  <td>{task.taskName}</td>
+                  <td>{task.description}</td>
+                  <td className={style.actionCol}>
+                    <button className={style.editBtn} onClick={()=> handleEdit(index)}>
+                      Update
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+
+        {openCanvas && (
+          <AddTask
+          taskName={taskName}
+          setTaskName={setTaskName}
+          description={description}
+          setDescription={setDescription}
+          setOpenCanvas={setOpenCanvas}
+          isEdit={isEdit}
+          setIsEdit={setIsEdit}
+          setEditIndex={setEditIndex}
+          onSave={handleSaveTask}
+          />
+        )}
+    </>
   )
 }
 
